3d8c9d1ee626f9e2ae290b8c2343a7111c71f9ae
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-19 10:35:30 +0500

    header add bars

--- template/header/script.js
+++ template/header/script.js
@@ -1,5 +1,25 @@
 let search = document.getElementById("header__search-onclick");
-let body = document.getElementsByTagName("body");
+let barsMenu = document.getElementById("header__bars-link-home");
+// let bars = document.getElementById("bars");
+
 function searchOnclick() {
-  search.style = "display: block";
+  search.style.height = "45.5px";
+  search.style.opacity = "1";
 }
+function hideBarsMenu() {
+  barsMenu.style.visibility = "visible";
+  barsMenu.style.height = "380px";
+}
+
+// function showBarsMenu() {
+//   barsMenu.style.visibility = "visible";
+// }
+
+// document.body.addEventListener("click", (e) => {
+//   console.log(e.target, bars, e.target !== barsMenu);
+//   if (e.target !== barsMenu && e.target === bars) {
+//     showBarsMenu();
+//   } else {
+//     hideBarsMenu();
+//   }
+// });
