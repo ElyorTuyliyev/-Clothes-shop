157a906884068e9ae4367924ed4e8e74c7350eeb
To'yliyev.Elyor <tuyliyevelyor@gmail.com>
2024-06-19 11:01:44 +0500

    header change in  bars

--- template/header/script.js
+++ template/header/script.js
@@ -1,25 +1,23 @@
 let search = document.getElementById("header__search-onclick");
 let barsMenu = document.getElementById("header__bars-link-home");
-// let bars = document.getElementById("bars");
+let isSearchVisible = false;
 
 function searchOnclick() {
-  search.style.height = "45.5px";
-  search.style.opacity = "1";
+  isSearchVisible = !isSearchVisible;
+  if (isSearchVisible) {
+    search.style.height = "45.5px";
+    search.style.opacity = "1";
+  } else {
+    search.style.height = "0";
+  }
 }
 function hideBarsMenu() {
-  barsMenu.style.visibility = "visible";
-  barsMenu.style.height = "380px";
+  isSearchVisible = !isSearchVisible;
+  if (isSearchVisible) {
+    barsMenu.style.visibility = "visible";
+    barsMenu.style.height = "380px";
+  } else {
+    barsMenu.style.visibility = "hidden";
+    barsMenu.style.height = "0";
+  }
 }
-
-// function showBarsMenu() {
-//   barsMenu.style.visibility = "visible";
-// }
-
-// document.body.addEventListener("click", (e) => {
-//   console.log(e.target, bars, e.target !== barsMenu);
-//   if (e.target !== barsMenu && e.target === bars) {
-//     showBarsMenu();
-//   } else {
-//     hideBarsMenu();
-//   }
-// });
